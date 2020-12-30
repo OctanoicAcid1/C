5b75f456c8f83e1be0cf6037b5eadc306f313f23
OctanoicAcid1 <2215239336@qq.com>
2020-12-30 21:07:02 +0800

    commit C语言程序设计

--- C语言程序设计/考试/作业&考试6/3 计算平均值（二）/计算平均值（二）.c
+++ C语言程序设计/考试/作业&考试6/3 计算平均值（二）/计算平均值（二）.c
@@ -4,17 +4,10 @@ int main(void)
 {
 	int i,n;
 	scanf("%d",&n);
-	if(n<1||n>100)
-	  return 0;
 	char work[1],ch[n][5];
 	work[0]=getchar();
 	for(i=0;i<n;i++){
-		int j=-1;
-		do{
-			j++;
-			ch[i][j]=getchar();
-		}while(ch[i][j]!=' '&&ch[i][j]!='\n');
-		ch[i][j]='\0';
+		scanf("%s",&ch[i]);
 	}
 	
 	for(i=0;i<n;i++){
@@ -26,12 +19,48 @@ int main(void)
 	}
 	
 	else{
-	    int j=0;
+	    int j=0,len,t;
 	    float num[n],sum=0;
 	    for(i=0;i<n;i++){
+	    	num[i]=0;
+		}
+	    for(i=0;i<n;i++){
 		    if(strcmp(ch[i],"n/a")!=0){
-			    num[j]=ch[i][0]-'0';
-			    j++;
+		    	if(ch[i][0]!='-'){
+		    		len=strlen(ch[i]);
+		    		int y,c,d,z[len];
+		    		d=len;
+		    		for(c=0;c<len;c++)
+		    		  z[c]=1;
+		    		for(c=0;c<len;c++){
+		    		    for(y=0;y<d-1;y++){
+		    			    z[c]*=10;
+					    }
+					    d--;
+					}
+		    		for(t=0;t<len;t++){
+			            num[j]+=(ch[i][t]-'0')*z[t];
+			        }
+			        j++;
+			    }
+			    else{
+			    	len=strlen(ch[i]);
+		    		int y,c,d,z[len];
+		    		d=len;
+		    		for(c=1;c<len;c++)
+		    		  z[c]=1;
+		    		for(c=1;c<len;c++){
+		    		    for(y=1;y<d-1;y++){
+		    			    z[c]*=10;
+					    }
+					    d--;
+					}
+		    		for(t=1;t<len;t++){
+			            num[j]+=(ch[i][t]-'0')*z[t];
+			        }
+			        num[j]=-num[j];
+			        j++;
+				}
 		    }
 	    }
 	    for(i=0;i<j;i++){
